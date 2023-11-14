d6119a33b8905e7592a4d0daaa9436a4fa55ba05
Pmoneyyyy <Perryblake121@gmail.com>
2023-11-14 18:16:15 -0500

    I finished my python

--- Test-your-luck.py
+++ Test-your-luck.py
@@ -1,20 +1,58 @@
-# Defining a variable 'temperature'
-temperature = 40
-# Creating a decision structure to check the value of 'temperature'
-if temperature < 15:
-    # Printing a message if temperature is less than 15.
-    print ("It's cold outside!")
-elif temperature > 30:
-    # Printing a message if temperature is greater than 30.
-    print ("It's hot outside!")
+import random
+
+levels = 10
+currentLevel = 1
+
+enemyNames = [
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+    "sdofjsdf",
+]
+
+def displayInstructions():
+    gameInstructions = "There are 10 levels. Player has to choose 1 or 2. If it's correct you move on to the next level."
+
+
+userResponse = input( "\n<<<<<< Hit enter to start: Press ENTER: >>>>>>" )
+
+
+userResponse = input( "Do you want a tutorial Y/N: " )
+
+
+
+while currentLevel <= levels:
+    if userResponse.lower() == "y":
+        displayInstructions()
+
+    currentLevel = currentLevel + 1
+
+    print("Good luck!!!")
+    computerRandomNumber = random.randint(1,2)
+    #print( computerRandomNumber ) # Cheating
+
+    userNumber = int(input( f"Monster {enemyNames[ currentLevel ]}>>>>>>>> What's your number?: " ) )
+
+    while userNumber not in [1, 2]:
+        print( "Don't be silly, type in 1 or 2" )
+
+    if computerRandomNumber == userNumber:
+        print( f"You're lucky, my buddy will get you in level {currentLevel}" )
+    else:
+        break
+
+if currentLevel >= 10:
+    print( "You won!!!!" )
 else:
-    # Printing a default message for other temperature values.
-    print("The weather is moderate.")
-
-
-level = 10
-
-if level == 10:
-    print ("you survived the night")
-elif level < 10:
-    input("1 or 2? ")
+    print( "Hahaha, we got you" )
+# if level == 10:
+#     print ("you survived the night")
+# elif level < 10:
+#     input("1 or 2? ")
